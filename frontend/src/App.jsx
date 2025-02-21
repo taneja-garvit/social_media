ee140a6300b8f1307d56207649b2a6c235605dd0
Garvit <taneja.garvit29@gmail.com>
2025-02-22 01:45:14 +0530

    updated frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react'
+import ChatPage from './components/ChatPage'
+import EditProfile from './components/EditProfile'
+import Home from './components/Home'
+import Login from './components/Login'
+import MainLayout from './components/MainLayout'
+import Profile from './components/Profile'
+import Signup from './components/Signup'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+
+const browserRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <ProtectedRoutes><MainLayout /></ProtectedRoutes>,
+    children: [
+      {
+        path: '/',
+        element: <ProtectedRoutes><Home /></ProtectedRoutes>
+      },
+      {
+        path: '/profile/:id',
+        element: <ProtectedRoutes> <Profile /></ProtectedRoutes>
+      },
+     
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
+])
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { user } = useSelector(store => store.auth);
+  const { socket } = useSelector(store => store.socketio);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (user) {
+      const socketio = io('http://localhost:8000', {
+        query: {
+          userId: user?._id
+        },
+        transports: ['websocket']
+      });
+      dispatch(setSocket(socketio));
+
+      // listen all the events
+      socketio.on('getOnlineUsers', (onlineUsers) => {
+        dispatch(setOnlineUsers(onlineUsers));
+      });
+
+      socketio.on('notification', (notification) => {
+        dispatch(setLikeNotification(notification));
+      });
+
+      return () => {
+        socketio.close();
+        dispatch(setSocket(null));
+      }
+    } else if (socket) {
+      socket.close();
+      dispatch(setSocket(null));
+    }
+  }, [user, dispatch]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={browserRouter} />
     </>
   )
 }
